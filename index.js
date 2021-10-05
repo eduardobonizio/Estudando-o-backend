0bd6720f5a22e72f647ec7611e1ccb58a8f93246
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-10-04 23:02:51 -0300

    Ajustes prettier

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { response } = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { response } = require("express");
 
-require('dotenv').config();
+require("dotenv").config();
 
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/', (_request, response) => {
-  response.status(200).send('Ok');
+app.get("/", (_request, response) => {
+  response.status(200).send("Ok");
 });
 
-app.post('/login', () => {
-  response.status(200).send('Ok')
+app.post("/login", () => {
+  response.status(200).send("Ok");
 });
 
 app.listen(PORT, () => {
-  console.log('Online');
+  console.log("Online");
 });
