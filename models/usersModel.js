dd1137f47bf9bc79a600ac1b54c368617b420c04
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-10-04 23:28:52 -0300

    Adiciona função de criação de usuário

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,18 +1,30 @@
 // const { ObjectID } = require('mongodb');
+// { _id: ObjectID(id) }
 const mongoConnection = require("./connection");
 
-const findUser = async (_id, name) => {
+const findUser = async (username) => {
   const usersCollection = await mongoConnection
     .connection()
     .then((db) => db.collection("users"));
 
-  const user = await usersCollection.findOne(
-    // { _id: ObjectID(id) },
-    { name: name }
-  );
+  const user = await usersCollection.findOne({ username: username });
+  return { user };
+};
+
+const createUser = async (username, password) => {
+  const usersCollection = await mongoConnection
+    .connection()
+    .then((db) => db.collection("users"));
+
+  const user = await usersCollection.insertOne({
+    username: username,
+    password: password,
+  });
+
   return { user };
 };
 
 module.exports = {
   findUser,
+  createUser,
 };
