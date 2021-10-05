ced19b3106293798e811e7f3af254b12ff912289
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-10-04 23:29:08 -0300

    Adiciona registro simples

---
+++ controller/register.js
@@ -0,0 +1,22 @@
+const UsersModel = require("../models/usersModel");
+
+const register = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password)
+      return res
+        .status(401)
+        .json({ message: "Usuário não existe ou senha inválida" });
+
+    await UsersModel.createUser(username, password);
+
+    return res.status(200).json({ message: "Usuário criado com sucesso" });
+  } catch (e) {
+    return res.status(500).json({ message: "Erro interno", error: e });
+  }
+};
+
+module.exports = {
+  register,
+};
