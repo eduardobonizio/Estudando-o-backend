ced3e98ec56cd4c0c55215b9d1d1119b567074a9
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-10-08 00:04:48 -0300

    Adiciona exemplo de post form com Axios e Form-Data

--- consume_api/index.js
+++ consume_api/index.js
@@ -1,16 +1,18 @@
 const axios = require("axios");
-
-const body = {
-  username: "Axios Test",
-  password: "Super Password",
-};
+const fs = require("fs");
+const formData = require("form-data");
 
 const axiosTest = async () => {
+  const body = {
+    username: "Axios Test",
+    password: "Super Password",
+  };
+
   await axios
     .get("http://localhost:3000/")
     .then((response) => {
-      console.log(response.data);
-      console.log(response.status);
+      console.log("axiosTest method get response.status", response.status);
+      console.log("axiosTest method get response.data", response.data);
     })
     .catch((error) => {
       console.log(error);
@@ -19,11 +21,45 @@ const axiosTest = async () => {
   await axios
     .post("http://localhost:3000/register", body)
     .then((response) => {
-      console.log(response.data);
+      console.log("axiosTest method post response.status", response.status);
+      console.log("axiosTest method post response.data", response.data);
     })
     .catch((error) => {
       console.log(error);
     });
 };
 
-axiosTest();
+const postFileWithAxios = async () => {
+  const stream = fs.createReadStream("./consume_api/sample.txt");
+
+  const form = new formData();
+
+  form.append("file", stream);
+
+  const formHeaders = form.getHeaders();
+
+  await axios
+    .post("http://localhost:3000/upload", form, {
+      headers: {
+        ...formHeaders,
+      },
+    })
+    .then((response) => {
+      console.log(
+        "postFileWithAxios method post response.status",
+        response.status
+      );
+      console.log("postFileWithAxios method post response.data", response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const runAll = async () => {
+  await axiosTest();
+
+  await postFileWithAxios();
+};
+
+runAll();
