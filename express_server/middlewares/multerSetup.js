379b41b00444d99a7c50285e1dbf80fe58331366
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-10-07 23:31:08 -0300

    Move os arquivos do servidor para a pasta express_server

--- express_server/middlewares/multerSetup.js
+++ express_server/middlewares/multerSetup.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 const uploadWithMulter = multer({
   storage: multer.diskStorage({
     destination: (req, file, callback) => {
-      callback(null, "uploads/");
+      callback(null, "express_server/uploads");
     },
     filename: (req, file, callback) => {
       callback(null, Date.now() + "-" + file.originalname);
