63b0849361306a13aae36a1572f1e3217a5075ea
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-10-07 22:05:29 -0300

    Remove importação do body-parser

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const Login = require("./controller/login");
 const Register = require("./controller/register");
 const Auth = require("./middlewares/auth");
